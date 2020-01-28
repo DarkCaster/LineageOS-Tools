f9af18f2b8396372fa40f3a6a921c3bb66a247c5
DarkCaster <dark.caster@outlook.com>
2020-01-29 02:52:41 +0300

    Custom_Builder_16 project: added script for generating sign-keys archive

---
+++ Custom_Builder_16/scripts/generate-keys.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+
+set -e
+
+show_usage() {
+  echo "usage: generate-keys.sh <lineage os source dir> <archive>"
+  exit 1
+}
+
+lineage_scrdir="$1"
+[[ -z $lineage_scrdir ]] && show_usage
+
+target_file="$2"
+[[ -z $target_file ]] && show_usage
+target_file=$(readlink -f "$target_file")
+
+self_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+rm -fv "$target_file"
+
+tmp_dir="$TMPDIR"
+[[ -z $tmp_dir || ! -d $tmp_dir ]] && tmp_dir="/tmp"
+tmp_dir=$(mktemp -d --tmpdir="$tmp_dir" keys.XXXXXXXXX)
+
+key_dir="$tmp_dir/keys"
+mkdir -p "$key_dir"
+
+pushd 1>/dev/null "$lineage_scrdir"
+subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+echo "Creating signing keys..."
+for key in releasekey platform shared media testkey; do
+  ./development/tools/make_key "$key_dir/$key" "$subject" || true
+  [[ ! -f "$key_dir/$key.pk8" || ! -f "$key_dir/$key.x509.pem" ]] && echo "failed to create key: $key" && exit 1
+done
+popd 1>/dev/null
+
+echo "Creating keys archive..."
+"$self_dir/create-archive.sh" "$key_dir" "$target_file"
+
+rm -rf "$tmp_dir"
