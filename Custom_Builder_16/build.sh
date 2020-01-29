93c62b55174f5bffe5472dfdc29fb47e895692c5
DarkCaster <dark.caster@outlook.com>
2020-01-29 04:41:37 +0300

    Custom_Builder_16 project: main build script updated

--- Custom_Builder_16/build.sh
+++ Custom_Builder_16/build.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 #
 
-### settings
+set -e
+
+show_usage() {
+  echo "usage: build.sh <lineage os srcdir> <device> <target (see below)> [additional parameters for target]"
+  exit 1
+}
 
 #lineage source directory
-lineage_scrdir="$1"
-[[ -z $lineage_scrdir ]] && lineage_scrdir="$HOME/android/lineage"
+lineage_srcdir="$1"
+[[ -z $lineage_srcdir ]] && show_usage
 
 #device
 target_device="$2"
-[[ -z $target_device ]] && target_device="river"
+[[ -z $target_device ]] && show_usage
+
+#what to build: ota - signed OTA update archive; vendor - populate vendor archive; keys - (re)create sign keys
+target="$3"
+[[ -z $target ]] && show_usage
+
+shift 3
+
+### other settings
+
+cleanup_srcdir="false"
+skip_patches="false"
 
 #embed su addon
 export WITH_SU="true"
@@ -19,26 +35,62 @@ with_fdroid="true"
 
 ### end of settings
 
-set -e
+[[ $target != "ota" ]] && skip_patches="true"
 
-[[ ! -d $lineage_scrdir ]] && echo "lineage source directory is missing :$lineage_scrdir" && exit 1
+[[ ! -d $lineage_srcdir ]] && echo "lineage source directory is missing :$lineage_srcdir" && exit 1
 
-self_dir="$( cd "$( dirname "$0" )" && pwd )"
+self_dir="$(cd "$(dirname "$0")" && pwd)"
 scripts_dir="$self_dir/scripts"
 
-#TODO: apply device specific patches
-source "$self_dir/quilt_set.sh.in"
-pushd 1>/dev/null "$lineage_scrdir"
-[[ -d $QUILT_PATCHES ]] && echo "applying patches from directory $patches_dir" && quilt push -a
-popd 1>/dev/null
-source "$self_dir/quilt_unset.sh.in"
+#TODO: cleanup src dir
+if [[ $cleanup_srcdir = true ]]; then
+  #TODO: clean src dir
+  #TODO: repo sync
+  echo "TODO"
+  exit 1
+fi
 
-#TODO: fdroid setup
-if [[ $with_fdroid = true ]]; then
-  patches_dir="fdroid"
+if [[ $skip_patches != true ]]; then
+
+  #TODO: apply device specific patches
+  patches_dir="patches-common"
   source "$self_dir/quilt_set.sh.in"
-  pushd 1>/dev/null "$lineage_scrdir"
+  pushd 1>/dev/null "$lineage_srcdir"
   [[ -d $QUILT_PATCHES ]] && echo "applying patches from directory $patches_dir" && quilt push -a
   popd 1>/dev/null
   source "$self_dir/quilt_unset.sh.in"
+
+  #TODO: fdroid setup
+  if [[ $with_fdroid = true ]]; then
+    patches_dir="fdroid"
+    source "$self_dir/quilt_set.sh.in"
+    pushd 1>/dev/null "$lineage_srcdir"
+    [[ -d $QUILT_PATCHES ]] && echo "applying patches from directory $patches_dir" && quilt push -a
+    popd 1>/dev/null
+    source "$self_dir/quilt_unset.sh.in"
+  fi
+else
+  echo "skipping applying patches"
 fi
+
+pushd 1>/dev/null "$lineage_srcdir"
+
+echo "preparing build env"
+source build/envsetup.sh
+
+echo "running $target target"
+if [[ $target = "vendor" ]]; then
+  vendor="$1"
+  [[ -z $vendor ]] && echo "please provide device vendor name as the last parameter" && show_usage
+  breakfast "$target_device" || echo "fail was expected at this stage..."
+  pushd 1>/dev/null "device/$vendor/$target_device"
+  echo "extracting vendor files"
+  ./extract-files.sh
+  popd 1>/dev/null
+  echo "creating new vendor-files archive"
+  "$self_dir/create-vendor-files-archive.sh" "$lineage_srcdir" "$vendor" "$self_dir/private/$target_device.enc"
+else
+  echo "unknown or unimplemented target: $target"
+fi
+
+pushd 1>/dev/null
