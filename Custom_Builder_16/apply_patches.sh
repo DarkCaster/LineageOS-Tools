bef724cdc6a1bdd057979960421427aa284342fd
DarkCaster <dark.caster@outlook.com>
2020-02-11 01:40:56 +0300

    Custom_Builder_16 project: apply_patches.sh updated - disable F-Droid Privileged Extension due to this bug: https://gitlab.com/fdroid/privileged-extension/issues/61

--- Custom_Builder_16/apply_patches.sh
+++ Custom_Builder_16/apply_patches.sh
@@ -17,7 +17,9 @@ device="$2"
 ### settings
 
 fdroid_url="https://f-droid.org/repo/org.fdroid.fdroid.privileged.ota_2110.zip"
+fdroid_url_standalone="https://f-droid.org/repo/org.fdroid.fdroid_1007051.apk"
 with_fdroid="true"
+with_fdroid_priv="false"
 
 ### end of settings
 
@@ -44,20 +46,26 @@ popd 1>/dev/null
 #fdroid patches
 if [[ $with_fdroid = true ]]; then
   mkdir -p "$lineage_srcdir/packages/apps/F-Droid"
-  mkdir -p "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
+  [[ $with_fdroid_priv = true ]] && mkdir -p "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
 
   #patch makefiles to include fdroid into build
   pushd 1>/dev/null "$lineage_srcdir"
   patches_dir="patches/fdroid"
   source "$self_dir/quilt_set.sh.in"
-  [[ -d $QUILT_PATCHES ]] && echo "processing patches from directory $patches_dir" && quilt push -a
+  [[ $with_fdroid_priv = true && -d $QUILT_PATCHES ]] && echo "processing patches from directory $patches_dir" && quilt push -a
+  [[ $with_fdroid_priv != true && -d $QUILT_PATCHES ]] && echo "processing patches from directory $patches_dir" && quilt push 1
   source "$self_dir/quilt_unset.sh.in"
   popd 1>/dev/null
 
   #download and install fdroid files
-  rm -rf "$self_dir/temp/workspace"
-  "$self_dir/../FDroid_OTA_Packager/scripts/01-download.sh" "$fdroid_url" "$self_dir/temp"
-  mv "$self_dir/temp/workspace/F-Droid.apk" "$lineage_srcdir/packages/apps/F-Droid"
-  mv "$self_dir/temp/workspace/F-DroidPrivilegedExtension.apk" "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
-  mv "$self_dir/temp/workspace/permissions_org.fdroid.fdroid.privileged.xml" "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
+  if [[ $with_fdroid_priv = true ]]; then
+    rm -rf "$self_dir/temp/workspace"
+    "$self_dir/../FDroid_OTA_Packager/scripts/01-download.sh" "$fdroid_url" "$self_dir/temp"
+    mv "$self_dir/temp/workspace/F-Droid.apk" "$lineage_srcdir/packages/apps/F-Droid"
+    mv "$self_dir/temp/workspace/F-DroidPrivilegedExtension.apk" "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
+    mv "$self_dir/temp/workspace/permissions_org.fdroid.fdroid.privileged.xml" "$lineage_srcdir/packages/apps/F-DroidPrivilegedExtension"
+  else
+    rm -fv "$lineage_srcdir/packages/apps/F-Droid/F-Droid.apk"
+    wget -O "$lineage_srcdir/packages/apps/F-Droid/F-Droid.apk" "$fdroid_url_standalone"
+  fi
 fi
