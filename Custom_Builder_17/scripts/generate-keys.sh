c1f301c95054443d4b11d5945366fccaffb72cf8
DarkCaster <dark.caster@outlook.com>
2020-05-06 02:14:11 +0300

    Custom_Builder_17 project: generate-keys.sh script updated

--- Custom_Builder_17/scripts/generate-keys.sh
+++ Custom_Builder_17/scripts/generate-keys.sh
@@ -29,7 +29,7 @@ mkdir -p "$key_dir"
 pushd 1>/dev/null "$lineage_srcdir"
 subject='/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
 echo "Creating signing keys..."
-for key in releasekey platform shared media testkey; do
+for key in releasekey platform shared media networkstack testkey; do
   ./development/tools/make_key "$key_dir/$key" "$subject" || true
   [[ ! -f "$key_dir/$key.pk8" || ! -f "$key_dir/$key.x509.pem" ]] && echo "failed to create key: $key" && exit 1
 done
