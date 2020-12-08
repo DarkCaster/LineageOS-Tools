c8cea345159b212f714b5aea816b5ba69ca5fcc0
DarkCaster <dark.caster@outlook.com>
2020-12-09 02:10:23 +0300

    MicroG_Lineage_Builder: lineageos.cfg.lua config updated, readme updated

--- MicroG_Lineage_Builder/README.md
+++ MicroG_Lineage_Builder/README.md
@@ -1,7 +1,7 @@
-# LineageOS + MicroG build scripts
+# LineageOS+MicroG builder
 
 Based on this project https://github.com/lineageos4microg/docker-lineage-cicd
 
 License: GPLv3
 
-_Experimental stuff, intended for use with my LXC-based CI env to build LineageOS + MicroG with my customizations. May be unstable and not work as intended, use original source (link above) in doubt._
+_Experimental stuff, intended for use with my Sandboxer software isolation/containerisation tool. May be unstable and not work as intended, use original source (link above) in doubt._
--- MicroG_Lineage_Builder/lineageos.cfg.lua
+++ MicroG_Lineage_Builder/lineageos.cfg.lua
@@ -32,6 +32,7 @@ loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.devshm_mount)
 -- /sys mount is needed for adb\fastboot to work, uncomment next line to disable it
 -- loader.table.remove_value(sandbox.setup.mounts,defaults.mounts.sys_mount)
 
+-- usb devfs mount needed for adb\fastboot to work
 table.insert(sandbox.setup.mounts,{prio=98,"dev-bind","/dev/bus/usb","/dev/bus/usb"})
 
 -- set hostname to "sandbox"
@@ -40,9 +41,11 @@ table.insert(sandbox.bwrap,defaults.bwrap.hostname_sandbox)
 -- add bwrap unshare_ipc option
 loader.table.remove_value(sandbox.bwrap,defaults.bwrap.unshare_ipc)
 table.insert(sandbox.bwrap,defaults.bwrap.unshare_ipc)
+
+-- disable creating new cgroup namespace for sandbox (TODO: remember, why exactly I need this)
 loader.table.remove_value(sandbox.bwrap,defaults.bwrap.unshare_cgroup)
 
--- add mounts with sources directory
+-- define mounts needed for CI scripts to work
 _MIRROR_DIR="/srv/mirror"
 _SRC_DIR="/srv/src"
 _TMP_DIR="/srv/tmp"
@@ -53,21 +56,22 @@ _KEYS_DIR="/srv/keys"
 _LOGS_DIR="/srv/logs"
 _USERSCRIPTS_DIR="/srv/userscripts"
 
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/mirror"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/src"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/tmp"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/ccache"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/zips"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/local_manifests"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/keys"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/logs"'});
-table.insert(sandbox.setup.commands,{'mkdir -p "'..loader.workdir..'/build/userscripts"'});
+-- create directories for that mounts at host-side
+table.insert(sandbox.setup.commands,{'\
+  for d in mirror src tmp ccache logs; do \
+    [[ ! -e  "'..loader.workdir..'/build/$d" ]] && mkdir -p "'..loader.workdir..'/build/$d" \
+  done; \
+  for d in out keys local_manifests userscripts; do \
+    [[ ! -e  "'..loader.workdir..'/$d" ]] && mkdir -p "'..loader.workdir..'/$d" \
+  done; \
+'});
 
--- setup /root dir with build scripts
+-- prepare build scripts
 table.insert(sandbox.setup.commands,{
   'rm -rf "${cfg[tunables.configdir]}/builder"',
   'mkdir -p "${cfg[tunables.configdir]}/builder"',
-  'cp -R "'..loader.workdir..'/src"/* "${cfg[tunables.configdir]}/builder"'
+  'cp -R "'..loader.workdir..'/CI_Scripts/src"/* "${cfg[tunables.configdir]}/builder"',
+  -- apply some patches to build script to work better inside sandboxer-container
 });
 table.insert(sandbox.setup.mounts,{prio=100,tag="_BUILDER","bind",loader.path.combine(tunables.configdir,"builder"),"/root"})
 
@@ -79,45 +83,48 @@ table.insert(sandbox.setup.commands,{
   'chmod a+x "${cfg[tunables.auto.user_path]}/bin/repo"',
 });
 
+-- add mounts for CI sctipts to work
 table.insert(sandbox.setup.mounts,{prio=99,tag="_SRV_DIR","tmpfs","/srv"})
 table.insert(sandbox.setup.mounts,{prio=100,tag="_MIRROR_DIR","bind",loader.path.combine(loader.workdir,"build","mirror"),_MIRROR_DIR})
 table.insert(sandbox.setup.mounts,{prio=100,tag="_SRC_DIR","bind",loader.path.combine(loader.workdir,"build","src"),_SRC_DIR})
 table.insert(sandbox.setup.mounts,{prio=100,tag="_TMP_DIR","bind",loader.path.combine(loader.workdir,"build","tmp"),_TMP_DIR})
 table.insert(sandbox.setup.mounts,{prio=100,tag="_CCACHE_DIR","bind",loader.path.combine(loader.workdir,"build","ccache"),_CCACHE_DIR})
-table.insert(sandbox.setup.mounts,{prio=100,tag="_ZIP_DIR","bind",loader.path.combine(loader.workdir,"build","zips"),_ZIP_DIR})
-table.insert(sandbox.setup.mounts,{prio=100,tag="_LMANIFEST_DIR","bind",loader.path.combine(loader.workdir,"build","local_manifests"),_LMANIFEST_DIR})
-table.insert(sandbox.setup.mounts,{prio=100,tag="_KEYS_DIR","bind",loader.path.combine(loader.workdir,"build","keys"),_KEYS_DIR})
+table.insert(sandbox.setup.mounts,{prio=100,tag="_ZIP_DIR","bind",loader.path.combine(loader.workdir,"out"),_ZIP_DIR})
+table.insert(sandbox.setup.mounts,{prio=100,tag="_LMANIFEST_DIR","bind",loader.path.combine(loader.workdir,"local_manifests"),_LMANIFEST_DIR})
+table.insert(sandbox.setup.mounts,{prio=100,tag="_KEYS_DIR","bind",loader.path.combine(loader.workdir,"keys"),_KEYS_DIR})
 table.insert(sandbox.setup.mounts,{prio=100,tag="_LOGS_DIR","bind",loader.path.combine(loader.workdir,"build","logs"),_LOGS_DIR})
-table.insert(sandbox.setup.mounts,{prio=100,tag="_USERSCRIPTS_DIR","bind",loader.path.combine(loader.workdir,"build","userscripts"),_USERSCRIPTS_DIR})
+table.insert(sandbox.setup.mounts,{prio=100,tag="_USERSCRIPTS_DIR","bind",loader.path.combine(loader.workdir,"userscripts"),_USERSCRIPTS_DIR})
 
 -- just comment-out this whole block if sandbox not working because some of required utilities missing
 -- currently, this params should work only with my setup
 sandbox.bwrap_cmd={
-  --"netns-runner.sh", "vde_air",
-  "ionice","-c","3",
-  "nice","-n","19",
-  "taskset","--cpu-list","3,4,5,6,7,8,9,10,11,15,16,17,18,19,20,21,22,23",
-  "bwrap"
+  -- "netns-runner.sh", "vde_air", -- use my script to run sandbox inside separate network namespace
+  "ionice","-c","3", -- set idle io-priority
+  "nice","-n","19", -- set lowest CPU priority
+  "taskset","--cpu-list","2,3,4,5,6,7,8,9,10,11,14,15,16,17,18,19,20,21,22,23", -- do not use 2 cores from my CPU for build, you config will be different
+  "bwrap" -- main bwrap utility
 }
 
+-- modify built-in "shell" exec profile
 shell.env_unset={"TERM","LANG","MAIL"}
-
 shell.env_set={
   {"TERM","xterm"},
   {"LANG","en_US.UTF-8"}
 }
 
+-- define exec-profile that will build lineage os
 build={
-  exec="/bin/bash",
-  args={"-l"},
+  exec="/bin/bash", -- TODO: change to /root/init.sh to run build right after invoke
+  args={},
   path=_SRC_DIR,
-  env_set={},
+  env_unset={"TERM","LANG","MAIL"},
+  env_set={}, -- defined below
   term_signal=defaults.signals.SIGHUP,
   exclusive=true,
   attach=true,
   pty=true,
   term_on_interrupt=true,
-  term_orphans=true,
+  term_orphans=true, -- will forsefully stop all left-over tasks on exit
 }
 
 -- define some variables, needed for build scripts
@@ -125,6 +132,7 @@ build.env_set={
   {"TERM","xterm"},
   {"LANG","en_US.UTF-8"},
 
+  -- define main ENV variables as in original Docker script
   {"MIRROR_DIR",_MIRROR_DIR},
   {"SRC_DIR",_SRC_DIR},
   {"TMP_DIR",_TMP_DIR},
@@ -135,21 +143,19 @@ build.env_set={
   {"LOGS_DIR",_LOGS_DIR},
   {"USERSCRIPTS_DIR",_USERSCRIPTS_DIR},
 
-  -- config options
+  -- various config options
   {"USE_CCACHE","1"},
   {"CCACHE_SIZE","50G"},
   {"CCACHE_EXEC","/usr/bin/ccache"},
-  {"BRANCH_NAME","lineage-17.1"},
-  {"DEVICE_LIST","river"}, --moto g7
+  {"BRANCH_NAME","lineage-17.1"}, -- OS version
+  {"DEVICE_LIST","river"}, -- model of the device
   {"RELEASE_TYPE","CUSTOM"},
   {"OTA_URL",""},
-  {"USER_NAME","LineageOS Buildbot"},
+  {"USER_NAME","buildbot"},
   {"USER_MAIL","sandbox@sandboxer.host"},
-  {"INCLUDE_PROPRIETARY","true"},
-  {"BUILD_OVERLAY","false"},
+  {"INCLUDE_PROPRIETARY","true"}, -- absolutely must have
   {"LOCAL_MIRROR","false"},
   {"CLEAN_OUTDIR","false"},
-  {"CRONTAB_TIME","now"},
   {"CLEAN_AFTER_BUILD","true"},
   {"WITH_SU","false"},
   {"ANDROID_JACK_VM_ARGS","-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"},
@@ -161,4 +167,8 @@ build.env_set={
   {"SIGNATURE_SPOOFING","restricted"},
   {"DELETE_OLD_ZIPS","0"},
   {"DELETE_OLD_LOGS","0"},
+
+  -- do not change these (unsupported when running with sandboxer)
+  {"BUILD_OVERLAY","false"},
+  {"CRONTAB_TIME","now"},
 }
