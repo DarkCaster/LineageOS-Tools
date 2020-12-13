106919e5ba07e4759240b2d5ebe5ef856fd22555
DarkCaster <dark.caster@outlook.com>
2020-12-13 04:52:52 +0300

    MicroG_Lineage_Builder: example build.sh updated

--- MicroG_Lineage_Builder/build.sh
+++ MicroG_Lineage_Builder/build.sh
@@ -2,4 +2,4 @@
 
 #example how to invoke sandboxer for building lineage os, and pass custom package names via cmdline
 
-sandboxer lineageos.cfg.lua build GmsCore GsfProxy FakeStore MozillaNlpBackend NominatimNlpBackend com.google.android.maps.jar FDroid QKSMS DAVx5 additional_repos.xml
+sandboxer lineageos.cfg.lua build GmsCore GsfProxy FakeStore MozillaNlpBackend NominatimNlpBackend com.google.android.maps.jar FDroid QKSMS DAVx5 OpenTasks additional_repos.xml
